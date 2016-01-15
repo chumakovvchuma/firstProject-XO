45983acd595655ddcb7ed01a51c78c9b5641bec6
chumakovvchuma <v.chumakovv@gmail.com>
2016-01-16 00:06:22 +0200

    test  add

--- tst/chu/xo/model/FieldTest.java
+++ tst/chu/xo/model/FieldTest.java
@@ -1,5 +1,7 @@
 package chu.xo.model;
 
+import chu.xo.model.exceptions.AlreadyOccupiedException;
+import chu.xo.model.exceptions.invalidPointException;
 import org.junit.Test;
 
 import java.awt.*;
@@ -29,9 +31,71 @@ public class FieldTest {
         final Figure actualFigure = field.getFigure(inputPoint);
 
         assertEquals(inputFigure,actualFigure);
+    }
 
+    @Test
+    public void testGetFigureWhenFigureIsNotSet() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,0);
 
+        final Figure actualFigure = field.getFigure(inputPoint);
 
+        assertNull(actualFigure);
+    }
+    @Test
+    public void testGetFigureWhenXIsLessThenZero() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(-1,0);
 
+        try {
+        field.getFigure(inputPoint);
+            fail();
+        } catch (final invalidPointException e){}
+
+    }
+    @Test
+    public void testGetFigureWhenYIsLessThenZero() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,-1);
+
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        } catch (final invalidPointException e){}
+
+    }
+    @Test
+    public void testGetFigureWhenXIsMoreThenSize() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(field.getSize() + 1,0);
+
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        } catch (final invalidPointException e){}
+
+    }
+    @Test
+    public void testGetFigureWhenYIsMoreThenSize() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,field.getSize() + 1);
+
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        } catch (final invalidPointException e){}
+
+    }
+    @Test
+    public void testSetFigureAlreadyOccupied() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,0);
+        final Figure inputFigure = Figure.O;
+
+        field.setFigure(inputPoint, inputFigure);
+        try {
+            field.setFigure(inputPoint, inputFigure);
+            fail();
+        } catch (final AlreadyOccupiedException e){}
     }
 }
